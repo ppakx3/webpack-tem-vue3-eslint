706b66e292b381452841bd874a8e34ff4a03705e
ppakx3 <noth7305@gmail.com>
2021-09-29 22:03:07 +0900

    webpack-tem-vue3@eslint

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1> {{ message }}</h1>
+  <HelloWorld/>
+</template>
+
+<script>
+  import HelloWorld from '~/components/Helloworld'
+
+  export default {
+    components : {
+      HelloWorld
+    },
+    data() {
+      return {
+        message: '뭐야 어려워!'
+      }
+    }
+  }
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template>
+  <img
+    src="~assets/logo.png"
+    alt="Hello" />
+  <div></div> 
+</template>
